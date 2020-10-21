5f657fda5e8e7666f8b6e4abb3bd913b96c8c758
Scott Blodgett <scottblodgett@gmail.com>
2020-10-20 20:42:25 -0400

    add in xray

--- src/Index.py
+++ src/Index.py
@@ -1,10 +1,12 @@
 # pip install requests -t ./
-import requests, json
+import requests
+import json
 import re
 import validators
 from urllib.parse import urlparse, parse_qsl
-import boto3
+from aws_xray_sdk.core import xray_recorder
 
+@xray_recorder.capture('## create_response')
 def response(msg, status_code):
 
     print(msg)
@@ -16,27 +18,30 @@ def response(msg, status_code):
     headers["Access-Control-Allow-Origin"] = "*"
 
     body = {}
-    body["url"] = url.scheme + "://" + url.netloc + "/"+ url.path
+    body["url"] = url.scheme + "://" + url.netloc + "/" + url.path
     body["qs"] = dict(parse_qsl(url.query))
-    
+
     return {
         "statusCode": status_code,
-        "isBase64Encoded" : False,
-        "headers" : headers,
+        "isBase64Encoded": False,
+        "headers": headers,
         "body": json.dumps(body)
     }
+
 
 def formaturl(url):
     if not re.match('(?:http|ftp|https)://', url):
         return 'http://{}'.format(url)
     return url
 
+
 # Define the client to interact with AWS Lambda
 client = boto3.client('lambda')
 
+
 def url_handler(event, context):
     #print('starting now')
-    
+
     try:
         #print("Log stream name:", context.log_stream_name)
         #print("Log group name:",  context.log_group_name)
@@ -49,27 +54,28 @@ def url_handler(event, context):
         if qs is not None and (qs.get('url', None) is not None):
             url = event["queryStringParameters"]["url"]
         else:
-            output = response({'message':  'No URL specified' }, 400)
+            output = response({'message':  'No URL specified'}, 400)
             return (output)
-            
+
         if len(url) > 0:
             url = formaturl(url)
         else:
-            return response({'message':  'No URL specified' }, 400)
-        
+            return response({'message':  'No URL specified'}, 400)
+
         if not validators.url(url):
-            return response({'message':  'Bad Url' }, 400)
+            return response({'message':  'Bad Url'}, 400)
 
         r = requests.get(url, allow_redirects=False)
 
         if (r.headers.get('Location', None) is None):
             output = url
         else:
-            print('there')
-            output = (r.headers['Location']) #not guaranteed to have Location
-            
-        output = response({'message':  output }, 200)
+            # print('there')
+            output = (r.headers['Location'])  # not guaranteed to have Location
 
+        output = response({'message':  output}, 200)
+
+        subsegment = xray_recorder.begin_subsegment('SNS')
         # Define the input parameters that will be passed
         # on to the child function
         inputParams = {
@@ -77,13 +83,14 @@ def url_handler(event, context):
             "subject": "This is the subject of the message.",
             "message": str(output)
         }
+        xray_recorder.end_subsegment()
 
         child = client.invoke(
-            FunctionName = 'arn:aws:lambda:us-east-2:294402561156:function:dsbPublishtoSNS',
-            InvocationType = 'RequestResponse',
-            Payload = json.dumps(inputParams)
+            FunctionName='arn:aws:lambda:us-east-2:294402561156:function:dsbPublishtoSNS',
+            InvocationType='RequestResponse',
+            Payload=json.dumps(inputParams)
         )
-     
+
         print(json.load(child['Payload']))
         print('\n')
 
